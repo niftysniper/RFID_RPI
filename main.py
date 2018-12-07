386db8ee705f98c551e47727e96d42fabf59fc99
Nabin <nabin.fbi@gmail.com>
2018-12-07 13:22:53 +0545

    meage with the id done, request remaining

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+import RPi.GPIO as GPIO 
+import SimpleMFRC522
+
 import requests
-import random
 import datetime
 
-ts = datetime.datetime.utcnow();
-UID = random.randint(0,99999999999)
-payLoad = {
-  'UID': UID,
-  'timeStamp' : ts
-}
-r = requests.get('https://httpbin.org/post', params = payLoad)
+reader = SimpleMFRC522.SimpleMFRC522()
+server = 'htpp://httpbin.org/post'
+while True:
 
-print(r.url)
+	try:
+		ts = datetime.datetime.utcnow()
+		UID, text = reader.read()
+		print("UID found")
+		print(UID)
+
+		payload = {
+			'UID' : UID,
+			'timeStamp' : ts
+		}
+
+		r = requests.get(server, params = payload)
+		print(r.url)
+	
+	finally:
+		GPIO.cleanup()
+
